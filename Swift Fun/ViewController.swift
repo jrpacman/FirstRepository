aa7951bd7edd0d947aa6473cdfe9cc391ad0f90b
David Zikovitz <jrpacman@mac.com>
2018-10-21 16:07:01 +0200

    Initial Commit

---
+++ Swift Fun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Fun
+//
+//  Created by David Zikovitz on 2018-10-21.
+//  Copyright © 2018 David Zikovitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
