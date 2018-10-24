0177034861217fc3dc0fc6cfb33aa2afd6017f06
David Zikovitz <jrpacman@mac.com>
2018-10-24 20:17:16 +0200

    FIRST ONE!

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -9,10 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var buttonCount = 0
+    @IBOutlet weak var moLoser: UILabel!
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        buttonCount = buttonCount + 1
+        
+        print(buttonCount)
+        
+        if buttonCount >= 1 {
+            view.backgroundColor = UIColor.green
+            moLoser.text = "He is breaking everything."
+        }
+        
+        if buttonCount >= 10 {
+            view.backgroundColor = UIColor.red
+            moLoser.text = "All your stuff is broken!"
+        }
+         }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+      
     }
 
 
